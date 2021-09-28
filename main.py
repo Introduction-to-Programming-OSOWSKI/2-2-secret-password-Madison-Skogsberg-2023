dcf388b7119c373db3e9cf086450c3d45efa80e3
Madison-Skogsberg-2023 <816418@mystma.org>
2021-09-28 09:04:30 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #WRITE YOUR CODE IN THIS FILE
+#define function
+#add parameters
+def password(x):
+#if then else
+    if x == "Knights19":
+        return "ACCESS GRANTED"
+    else:
+        return "ACCESS DENIED"
+#run function
+print(password("Knights39"))
